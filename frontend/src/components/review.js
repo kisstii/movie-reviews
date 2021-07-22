3495a78f5380988deeeff10997038e9c9679a026
Tamas Kiss <kisstii1@gmail.com>
2021-07-22 11:55:58 +0200

    send review

--- frontend/src/components/review.js
+++ frontend/src/components/review.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import ReviewForm from "./reviewForm";
+
 function Review({ movie_id }) {
+  const [showReviewForm, setShowReviewForm] = useState(false);
+
   return (
-    <div className="reviewButtonContainer">
-      <button className="reviewButton">write a review</button>
-      <button className="reviewButton">read reviews</button>
-    </div>
+    <>
+      <div className="reviewButtonContainer">
+        <button className="reviewButton" onClick={() => setShowReviewForm(true)}>
+          write a review
+        </button>
+        <button className="reviewButton">read reviews</button>
+      </div>
+      {showReviewForm && <ReviewForm movie_id={movie_id} setShowReviewForm={setShowReviewForm} />}
+    </>
   );
 }
 
--- frontend/src/components/reviewForm.js
+++ frontend/src/components/reviewForm.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-function ReviewForm({ movie_id }) {
+function ReviewForm({ movie_id, setShowReviewForm }) {
   const [newReview, setNewReview] = useState("");
 
   const createNewReview = () => {
     const token = localStorage.getItem("accessToken");
-    fetch(`/api/review/movie${movie_id}`, {
+    fetch(`http://localhost:8000/api/review/movie/${movie_id}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -17,7 +17,9 @@ function ReviewForm({ movie_id }) {
     })
       .then((response) => response.json())
       .then((result) => {
+        console.log(result);
         setNewReview("");
+        setShowReviewForm(false);
       })
       .catch((error) => {
         console.error(error);
@@ -25,16 +27,14 @@ function ReviewForm({ movie_id }) {
   };
 
   return (
-    <>
-      <div className="formContainer">
-        <form className="form">
-          <textarea className="inputData" type="text" placeholder="write a review" value={newReview} onInput={(e) => setNewReview(e.target.value)} />
-          <button type="submit" className="actionButton" onClick={createNewReview}>
-            Add review
-          </button>
-        </form>
-      </div>
-    </>
+    <div className="reviewContainer">
+      <form className="review">
+        <textarea className="reviewData" type="text" placeholder="write a review" value={newReview} onInput={(e) => setNewReview(e.target.value)} />
+        <button type="button" className="reviewActionButton" onClick={createNewReview}>
+          send review
+        </button>
+      </form>
+    </div>
   );
 }
 
