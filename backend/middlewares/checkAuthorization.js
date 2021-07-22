0d51e5a1c5c43b7d00c62a6644e30304c9f6aa19
Klima András <klima.andras@gmail.com>
2021-07-22 09:50:18 +0200

    be api auth fix

--- backend/middlewares/checkAuthorization.js
+++ backend/middlewares/checkAuthorization.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
-const verifyJwt = (token) => {
+const verifyJwt = (token) => {  
   return new Promise((resolve, reject) => {
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
       if (err) return reject(err);
@@ -11,11 +11,11 @@ const verifyJwt = (token) => {
 };
 
 module.exports = asyncHandler(async (req, res, next) => {
-  if (!process.env.JWT_NOCHECK) {
-    try {
-      const authorizationHeader = req.headers["authorization"];
-      const token = authorizationHeader.split(" ")[1];
-      req.user = await verifyJwt(token);
+  if (process.env.JWT_NOCHECK === "false") {
+    try {      
+      const authorization = req.headers["authorization"];
+      req.user = await verifyJwt(authorization);
+
     } catch (err) {
       throw { message: "Unauthorized", status: 401 };
     }
